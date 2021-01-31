0c4665db9b0c3483bc70fa2aaf0671cf48e2e7f1
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-31 16:52:09 +0100

    Cmdlets

--- ProductivityTools.TrainingLog.SportsTracker/ExportTrainingsToSportsTracker/ExportTrainingsToSportsTrackerCmdlet.cs
+++ ProductivityTools.TrainingLog.SportsTracker/ExportTrainingsToSportsTracker/ExportTrainingsToSportsTrackerCmdlet.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Management.Automation;
 
 namespace ProductivityTools.TrainingLog.SportsTracker
 {
+    [Cmdlet("Export","TrainingsToSportTracker")]
     public class ExportTrainingsToSportsTrackerCmdlet : ProductivityTools.PSCmdlet.PSCmdletPT
     {
         protected override void ProcessRecord()
         {
+            WriteVerbose("Hello from Export-TrainingsToSportTracker");
             base.ProcessRecord();
         }
     }
--- ProductivityTools.TrainingLog.SportsTracker/ImportTrainingsFromSportsTracker/ImportTrainingsFromSportsTrackerCmdlet.cs
+++ ProductivityTools.TrainingLog.SportsTracker/ImportTrainingsFromSportsTracker/ImportTrainingsFromSportsTrackerCmdlet.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Management.Automation;
 using System.Text;
 
 namespace ProductivityTools.TrainingLog.SportsTracker.ImportTrainingsFromSportsTracker
 {
+    [Cmdlet("Import", "TrainingsFromSportTracker")]
     class ImportTrainingsFromSportsTrackerCmdlet : ProductivityTools.PSCmdlet.PSCmdletPT
     {
         protected override void ProcessRecord()
         {
+            WriteVerbose("Hello from Import-TrainingsToSportTracker");
             base.ProcessRecord();
         }
     }
